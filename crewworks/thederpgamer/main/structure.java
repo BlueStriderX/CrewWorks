2e0c308a1f1cfb5632263e92ab1b500e78cfd4c1
TheDerpGamer <garret2kool@gmail.com>
2019-03-22 13:45:18 -0700

    Update structure.java

--- crewworks/thederpgamer/main/structure.java
+++ crewworks/thederpgamer/main/structure.java
@@ -3,13 +3,7 @@ package crewworks.thederpgamer.main;
 import thederpgamer.main.main;
 import thederpgamer.main.blocks;
 
-public class structure {
-
-  public static Entity entity = new entity();
-  public static Block block = new block();
-  public static Structure structure = new structure();
-  public static Player player = new player();
-  public static Faction faction = player.getfaction();
+public class event {
 
   @EventHandler //starmade api equivalent?
   public static void onBlockPlace(EventHandler event, Block block) {
