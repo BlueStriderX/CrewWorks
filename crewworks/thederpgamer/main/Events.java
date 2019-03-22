e480046d86205f14851cfa53bb1dea884f670a0f
TheDerpGamer <garret2kool@gmail.com>
2019-03-22 14:05:05 -0700

    Create Events.java

---
+++ crewworks/thederpgamer/main/Events.java
@@ -0,0 +1,20 @@
+package crewworks.thederpgamer.main;
+
+import thederpgamer.main.Main;
+
+public class Events {
+
+  @EventHandler //starmade api equivalent?
+  public static void onBlockPlace(EventHandler event, Block block) {
+    if (block == blocks.crewModule) {
+      if (entity.type != "ship" || entity.type != "station") {
+        MessageBox placeError = new MessageBox;
+        placeError.setText("You can only put crew modules on ships or stations!", center)
+        placeError.addbutton("Ok", center)
+        player.sendMessage().placeError;
+        event.cancel(); //cancel block place event
+        return;
+      }
+    }
+  }
+}
