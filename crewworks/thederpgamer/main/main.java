dc2ee0c93843651fe407af702114b2aa82d85938
TheDerpGamer <garret2kool@gmail.com>
2019-03-22 13:25:58 -0700

    Create main.java

---
+++ crewworks/thederpgamer/main/main.java
@@ -0,0 +1,15 @@
+package crewworks.thederpgamer.main;
+
+import thederpgamer.crews.structure;
+
+public class main {
+
+  public static file mod = modinfo.json;
+  public static this.MODNAME = mod.MODNAME;
+  public static this.MODVERSION = mod.MODVERSION;
+  public static this.MODAUTHOR = mod.MODAUTHOR;
+  public static this.MODDESCRIPTION = mod.MODDESCRIPTION;
+
+  public static void onInitialize() {
+  }
+}
