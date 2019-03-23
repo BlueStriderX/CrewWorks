c6853f38bdc95954a03358c193386548a73e4f11
TheDerpGamer <garret2kool@gmail.com>
2019-03-23 11:42:46 -0700

    Create Blocks.java

---
+++ crewworks/thederpgamer/libraries/Blocks.java
@@ -0,0 +1,7 @@
+package crewworks.thederpgamer.libraries;
+
+import thederpgamer.libraries.Block;
+
+public class Blocks extends Block {
+  //this will read the vanilla block and custom block configs and add them
+}
