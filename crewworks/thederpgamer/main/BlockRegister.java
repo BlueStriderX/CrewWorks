7c49644a3d167d4d2780ef76db15a07ad4a5bbd3
TheDerpGamer <garret2kool@gmail.com>
2019-03-24 14:12:31 -0700

    Update BlockRegister.java

--- crewworks/thederpgamer/main/BlockRegister.java
+++ crewworks/thederpgamer/main/BlockRegister.java
@@ -6,6 +6,8 @@ import thederpgamer.blocks.Crew_Module;
 public class BlockRegister extends Block {
 
   public static registerBlocks() {
+    createCategory(Crew);
+
     customBlocks.add(Crew_Module.CREW_MODULE);
     registerConfigs(Crew_Module.CREW_MODULE);
   }
