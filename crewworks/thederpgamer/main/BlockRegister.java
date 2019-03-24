69548e20c2f302198d06e7a455096186870ce66b
TheDerpGamer <garret2kool@gmail.com>
2019-03-24 12:51:12 -0700

    Create BlockRegister.java

---
+++ crewworks/thederpgamer/main/BlockRegister.java
@@ -0,0 +1,12 @@
+package crewworks.thederpgamer.main;
+
+import thederpgamer.libraries.Block;
+import thederpgamer.blocks.Crew_Module;
+
+public class BlockRegister extends Block {
+
+  public static registerBlocks() {
+    customBlocks.add(Crew_Module.CREW_MODULE);
+    registerConfigs(Crew_Module.CREW_MODULE);
+  }
+}
