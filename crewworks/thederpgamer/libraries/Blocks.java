6352a63b8c8ae4a2dc17511392ce32eb9a546459
TheDerpGamer <garret2kool@gmail.com>
2019-03-25 08:47:37 -0700

    Delete Blocks.java

--- crewworks/thederpgamer/libraries/Blocks.java
+++
@@ -1,7 +0,0 @@
-package crewworks.thederpgamer.libraries;
-
-import thederpgamer.libraries.Block;
-
-public class Blocks extends Block {
-  //this will read the vanilla block and custom block configs and add them
-}
